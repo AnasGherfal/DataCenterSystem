dbc933a2b1837337f160a77ed397321ce7b2bd7e
F.Mansur <frganily@gmail.com>
2023-05-02 12:33:17 +0200

    DI

--- ManagementAPI/DI/ServicesExtension.cs
+++ ManagementAPI/DI/ServicesExtension.cs
@@ -7,6 +7,11 @@ public static class ServicesExtension
     public static void AddServices(this IServiceCollection services)
     {
         services.AddScoped<ICustomerService, CustomerService>();
+        services.AddScoped<IRepresentiveService, RepresentiveService>();
+        services.AddScoped<ICustomerFileService, CustomerFileService>();
+        services.AddScoped<IVisitService, VisitService>();
+        services.AddScoped<IVisitTimeShiftService, VisitTimeShiftService>();
+        services.AddScoped<ICompanionService, CompanionService>();
         services.AddScoped<ServiceServices>();
     }
 }

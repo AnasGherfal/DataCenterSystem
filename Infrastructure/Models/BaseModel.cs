92444629723d5657a6876e967676007498dd61b6
AOFDev <125885507+AmeenOmarf@users.noreply.github.com>
2023-08-21 11:46:23 +0200

    CustomerVisit

---
+++ Infrastructure/Models/BaseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Models;
+
+public abstract class BaseModel
+{
+    public DateTime CreatedOn { get; set; }
+    public Guid? CreatedById { get; set; } = Guid.Parse("c9dc9585-46bf-4a7c-f2f3-08db9bd865d4");
+    public User CreatedBy { get; set; } = default!;
+}

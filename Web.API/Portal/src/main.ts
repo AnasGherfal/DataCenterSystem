c29708c0feb243f6fb7673f1201fc3b960137b88
AnasGherfal <79544599+AnasGherfal@users.noreply.github.com>
2023-11-12 13:33:02 +0200

    IDK

--- Web.API/Portal/src/main.ts
+++ Web.API/Portal/src/main.ts
@@ -4,7 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
-//aassss
+//allkkll
 
 //style
 import "./assets/style/style.scss";
